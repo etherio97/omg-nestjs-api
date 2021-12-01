b98b8b15ba9a5094a7c86465c1b8249e8cd8b5ff
Shin Maung Maung <shinmaungmaung@mytel.com.mm>
2021-12-01 13:48:52 +0630

    a

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,12 @@ import { NestFactory } from "@nestjs/core";
 import * as helmet from "helmet";
 import * as cors from "cors";
 import { AppModule } from "./app.module";
+import { env } from "process";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
   app.use(cors());
-  await app.listen(3000);
+  await app.listen(env.PORT || 3000);
 }
 bootstrap();
